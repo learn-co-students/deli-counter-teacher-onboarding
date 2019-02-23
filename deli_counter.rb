9181c62dc43329d498a65cd27fc2c9f5ed20b676
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-23 22:48:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line deli
-  if deli.length == 0
+  if deli.empty?
     puts 'The line is currently empty.'
   else
     names = ""
-    for number in 1..deli.length
-      names = "#{names} #{number}. #{deli[number - 1]}"
+    deli.each.with_index(1) do |name, number|
+      names << " #{number}. #{name}"
     end
 
     puts "The line is currently:#{names}"
@@ -17,10 +17,9 @@ def take_a_number deli, name
 end
 
 def now_serving deli
-  if deli.length == 0
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    person = deli.shift
-    puts "Currently serving #{person}."
+    puts "Currently serving #{deli.shift}."
   end
 end
